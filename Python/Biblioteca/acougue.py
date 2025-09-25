0a621e7aa130b0635c53bd201250570c1ae1021c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-25 09:31:58 -0300

    continuação_açõugue

--- Python/Biblioteca/acougue.py
+++ Python/Biblioteca/acougue.py
@@ -1,10 +1,10 @@
 def forca_opcao(msg,lista_opcoes):
     msg += '\n'.join(lista_opcoes) + '\n->'
-    opcao = input(msg)
+    opcao = input(f"{msg}")
     while opcao not in lista_opcoes:
-        print("Erro!")
-        opcao = input(msg)
-    return(opcao)
+        print("Inválido!")
+        opcao = input(f"{msg}")
+    return opcao
 
 def acha_indice(lista,elem):
     for i in range(len(lista)):
@@ -32,6 +32,33 @@ def info_carne(dic):
         print(f"{key} : {acougue[key][indice_escolha]}")
     return info_carne
 
+def cadastrar():
+    for key in acougue.keys():
+        info = input(f"Diga o novo {key}:\n->")
+        acougue[key].append(info)
+    return
+
+def cria_indices():
+    indices = {acougue['Carnes'][i] : i for i in range(len(acougue['Carnes']))}
+    return indices
+
+def remover():
+    escolha = forca_opcao("Qual item será removido?\n",acougue['Carnes'])
+    indice_escolha = indices[escolha]
+    for key in acougue.keys():
+        acougue[key].pop(indice_escolha)
+    return
+
+def atualizar():
+    item = forca_opcao("Qual item você deseja atualizar?\n->",acougue['Carnes'])
+    indice_item = indices[item]
+    for key in acougue.keys():
+        if forca_opcao(f"Você quer atualizar {key} para {item}?\n", ['sim','não']) == 'sim':
+            info = input(f"DIga o novo {key}: ")
+            acougue[key][indice_item] = info
+        return
+
+
 
 acougue = {
     'Carnes' : ['Patinho','Coxão Mole','Fraldinha','Picanha','Maminha','Linguiça'],
@@ -39,6 +66,8 @@ acougue = {
     'Estoque' : [10,50,30,15,20,100],
     'Validade' : ['4','7','5','9','20','50'],
 }
+
+indices = cria_indices()
 
 menu = '''
 Bem-Vindo ao Açougue do Danilo 😁
@@ -53,5 +82,13 @@ print(menu)
 acao = input("O que você deseja fazer?\n->")
 if acao == '1':
     info_carne(acougue)
-if acao == '2':
+elif acao == '2':
+    cadastrar()
+    indice = cria_indices()
+elif acao == '3':
+    remover()
+    indice = cria_indices()
+elif acao == '4':
+    atualizar()
+else:
     print()
